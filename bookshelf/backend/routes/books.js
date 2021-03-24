193e8d6a38622b132fac71ea46d0052d1dac031c
Shamanth087 <11013363@stud.hochschule-heidelberg.de>
2021-03-24 16:44:30 +0100

    crud done

--- bookshelf/backend/routes/books.js
+++ bookshelf/backend/routes/books.js
@@ -26,4 +26,32 @@ router.route('/add').post((req,res) =>{
 
 });
 
+router.route('/:id').get((req,res)=>{
+    Book.findById(req.params.id)
+    .then(book =>res.json(book))
+    .catch(err =>res.status(400).json('Error:'+err));
+});
+
+router.route('/:id').delete((req,res)=>{
+    Book.findByIdAndDelete(req.params.id)
+    .then(() =>res.json("book deleted"))
+    .catch(err =>res.status(400).json('Error:'+err));
+});
+
+router.route('/update/:id').post((req,res)=>{
+    Book.findById(req.params.id)
+    .then(book => {
+        book.title=req.body.title;
+        book.isbn=req.body.isbn;
+        book.author=req.body.author;
+        book.annotation=req.body.annotation;
+        
+        book.save()
+        .then(() =>res.json("book updated"))
+        .catch(err =>res.status(400).json('Error:'+err));
+
+    })
+    .catch(err =>res.status(400).json('Error:'+err));
+});
+
 module.exports=router;
